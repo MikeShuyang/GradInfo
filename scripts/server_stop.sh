750bbd5e53db8d14ac2e1cc10025e748d88d359a
runnan <runnando@usc.edu>
2021-11-28 23:18:30 -0800

    sdadasd

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
-sudo pkill -f 'java-jar'
+#!/bin/bash
+sudo pkill -f 'java -jar'
--- src/main/java/com/example/gradinfo/dto/request/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.gradinfo.dto.request;
-
-import lombok.Data;
-
-@Data
-public class StudentInfo {
-    private String studentId;
-    private String spPostNumber;
-}
