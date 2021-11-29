8af386ae582b5e8e26a2f493e5823b2ddd281ec3
runnan <runnando@usc.edu>
2021-11-29 00:06:26 -0800

    HelloThere

---
+++ src/main/java/com/example/gradinfo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.gradinfo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("api/health")
+    public ResponseEntity<?> healthCheck()
+    {
+        return ResponseEntity.ok("It works successfully.");
+    }
+}
