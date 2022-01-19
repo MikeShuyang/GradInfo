3f8df70df08a3c7fc2d002bf3a5cb14f870911fe
MikeShuyang <wangshuy@usc.edu>
2022-01-19 15:31:18 -0800

    Enabled Swagger

--- src/main/java/com/example/gradinfo/GradInfoApplication.java
+++ src/main/java/com/example/gradinfo/GradInfoApplication.java
@@ -2,8 +2,10 @@ package com.example.gradinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableOpenApi
 public class GradInfoApplication {
 
     public static void main(String[] args) {
